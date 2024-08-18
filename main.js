d0f8e2a8c2cd7e8de2ac08ddc693c145b6ba7e90
FarazAhmad2 <farazahmad574@gmail.com>
2024-08-18 21:54:00 +0530

    first commit

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const fs = require('fs');
 
 function printFileContents(filePath) {
   // TODO: Use fs.readFile to read the file contents
+  fs.readFile(filePath,(err, data)=>{
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.toString());
+    }
+  })
 }
 
 // TODO: Call printFileContents with the command-line argument
+printFileContents('./output.txt')
